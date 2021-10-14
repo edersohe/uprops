f9920d3467f2c02b6e1a2bf525a85c5c58ae3b51
Eder Sosa <eder.sohe@gmail.com>
2021-10-14 00:29:05 -0500

    improvement used only regular expressions that supports multiline values

--- uprops.go
+++ uprops.go
@@ -4,37 +4,16 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strings"
 )
 
-var (
-	trimSpaces  = true
-	separatorKV = "="
-	commentChar = "#"
-	eol         = "\n"
-)
-
-func dos2Unix(s string) string {
-	var re = regexp.MustCompile(`(?m:\r\n?`)
-	return re.ReplaceAllString(s, "\n")
+func getProperties(s string) [][]string {
+	var re = regexp.MustCompile(`(?m)^[\t ]*([^#\r\n].+[^\t ])[\t ]*=((?:.*\\\s*\n)*.*)$`)
+	return re.FindAllStringSubmatch(s, -1)
 }
 
-func getLines(s string) []string {
-	return strings.Split(s, eol)
-}
-
-func clean(s string) string {
-	var re = regexp.MustCompile(`(?m:(^[ \t]+|[ \t]+$)`)
-	return re.ReplaceAllString(s, "")
-}
-
-func getPropName(s string) string {
-	return clean(strings.SplitN(s, separatorKV, 2)[0])
-}
-
-func replace(s string, l string) string {
-	var re = regexp.MustCompile(`(?m:^[\t ]*` + regexp.QuoteMeta(getPropName(l)) + `[\t ]*=.*$)`)
-	return re.ReplaceAllString(s, l)
+func replace(s string, k string, v string) string {
+	var re = regexp.MustCompile(`(?m)^[\t ]*` + regexp.QuoteMeta(k) + `[\t ]*=(?:.*\\\s*\n)*.*$`)
+	return re.ReplaceAllString(s, v)
 }
 
 func main() {
@@ -44,15 +23,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	strBase := dos2Unix(args[1])
-	strCustom := dos2Unix(args[2])
+	strBase := args[1]
+	strCustom := args[2]
 	strMerge := strBase
 
-	for _, l := range getLines(strCustom) {
-		l = clean(l)
-		if l != "" && !strings.HasPrefix(l, commentChar) && strings.Contains(l, separatorKV) {
-			strMerge = replace(strMerge, l)
-		}
+    properties := getProperties(strCustom)
+	for i := range properties {
+        //fmt.Printf("property: %s\n\nkey: %s\n\nvalue: %s\n\n\n", properties[i][0], properties[i][1], properties[i][2])
+        strMerge = replace(strMerge, properties[i][1], properties[i][0])
 	}
 
 	fmt.Println(strMerge)
